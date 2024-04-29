88f47fdfa6d8493802c4b835d987aa37fc894b06
Matt Daniel Brown <mattbrown@onmail.com>
2024-04-29 06:53:03 -0500

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), alpinejs(), image(), mdx()]
 });
